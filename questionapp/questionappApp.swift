9d6965008359743bacd5fa1f5391825a295f5db3
Michelle Elias Flores <michelleeliasflores@gmail.com>
2023-05-02 17:33:46 -0400

    Initial Commit

---
+++ questionapp/questionappApp.swift
@@ -0,0 +1,17 @@
+//
+//  questionappApp.swift
+//  questionapp
+//
+//  Created by Michelle Elias Flores on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct questionappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
