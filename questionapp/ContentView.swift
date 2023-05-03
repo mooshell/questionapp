728a044e188a7788074a1658cd302ca2cc0cb70f
Michelle Elias Flores <michelleeliasflores@gmail.com>
2023-05-03 18:55:44 -0400

    new edits

--- questionapp/ContentView.swift
+++ questionapp/ContentView.swift
@@ -2,20 +2,180 @@
 //  ContentView.swift
 //  questionapp
 //
-//  Created by Michelle Elias Flores on 5/2/23.
+//  Created by Michelle Elias Flores on 5/1/23.
 //
 
 import SwiftUI
-
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            FirstView()
         }
-        .padding()
+    }
+}
+
+struct FirstView: View {
+    @State private var goToSecondView: Bool = false
+    @State private var selectedText: String = ""
+
+    var body: some View {
+        VStack {
+            Text("Pick a coffee!")
+                .font(.largeTitle)
+                .foregroundColor(.yellow)
+            // Add the three buttons with custom messages
+                       Button(action: {
+                           selectedText = "I love coffee! Lattes are awesome!"
+                       }) {
+                           Text("COFFEE!")
+                               .padding()
+                               .foregroundColor(.white)
+                               .background(Color.orange)
+                               .cornerRadius(10)
+                       }
+                       .padding()
+                        Button(action: {
+                           Image("icecoffe")
+                       }) {
+                           Text("Iced Coffee!")
+                               .padding()
+                               .foregroundColor(.white)
+                               .background(Color.blue)
+                               .cornerRadius(10)
+                       }
+                       .padding()
+
+            Button(action: {
+                selectedText = "milkshakes and other things are also cool!"
+            } ) {
+                           Text("NO COFFEEE PLEASE!")
+                               .padding()
+                               .foregroundColor(.white)
+                               .background(Color.green)
+                               .cornerRadius(10)
+                       }
+
+                       Spacer()
+            HStack {
+                Spacer()
+                NavigationLink(destination: SecondView(), isActive: $goToSecondView) {
+                    EmptyView()
+                }
+                Button(action: {
+                    goToSecondView = true
+                }) {
+                    Text("Next")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.yellow)
+                        .cornerRadius(10)
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+struct SecondView: View {
+    @State private var goToThirdView: Bool = false
+
+    var body: some View {
+        VStack {
+            Text("What is your favorite color!!")
+                .font(.largeTitle)
+                .foregroundColor(.pink)
+            Button(action: {
+                print("That is also my favorite color!")
+            }) {
+                Text("PINK!")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.orange)
+                    .cornerRadius(10)
+            }
+            .padding()
+             Button(action: {
+                print("Do you like the beach?")
+            }) {
+                Text("BLUE!")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding()
+
+            Button(action: {
+                print("Black is a cool color")
+            }) {
+                Text("BLACK")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.green)
+                    .cornerRadius(10)
+            }
+
+            Spacer()
+            HStack {
+                Spacer()
+                NavigationLink(destination: ThirdView(), isActive: $goToThirdView) {
+                    EmptyView()
+                }
+
+                Button(action: {
+                    goToThirdView = true
+                }) {
+                    Text("Next")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.pink)
+                        .cornerRadius(10)
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+struct ThirdView: View {
+    var body: some View {
+        VStack {
+            Text("How are you feeling now?")
+                .font(.largeTitle)
+                .foregroundColor(.red)
+            Button(action: {
+                print("Stay with that happy energy!!")
+            }) {
+                Text("Happy")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.orange)
+                    .cornerRadius(10)
+            }
+            .padding()
+             Button(action: {
+                print("Here is a heart to make you feel better <3")
+            }) {
+                Text("Sad")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding()
+
+            Button(action: {
+                print("YOU ARE AMAZINGGGG")
+            }) {
+                Text("Nervous")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.green)
+                    .cornerRadius(10)
+            }
+
+            Spacer()
+        }
     }
 }
 
@@ -24,3 +184,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
